1373965a91c235d03b737e35b3aec536d3b8e15d
Abhijeet3010 <abhijeetsingh2093@gmail.com>
2020-12-27 18:52:27 +0530

    comments added

--- audioDL.py
+++ audioDL.py
@@ -1,12 +1,17 @@
+# This is a addon module to help download songs from
+# the discord bot, Porygon.
+
 import os
 
 
 def song_download(link):
+    # Downloads songs via link
     command = f"youtube-dl -o DL_songs/%(title)s.%(ext)s -x --audio-format mp3 {link}"
     return os.system(command)
 
 
 def search_download(usersearch):
+    # Downloads songs via searching the most relevant one in the ytsearch.
     command = str(
         r'youtube-dl -x --audio-format mp3 -o DL_songs/%(title)s.%(ext)s ytsearch:"' + usersearch + r'"')
     return os.system(command)
--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
-from logging import error
+# This is a discord bot called Porygon
+# initially created to download songs
+# but soon turned in to a tormenting bot
+# for Bhavi
+
 import discord
 from discord.ext import commands
 import time
@@ -14,26 +18,37 @@ client = commands.Bot(command_prefix=">")
 
 @client.event
 async def on_ready():
+    # passes the "ready" comment in the terminal
+    # on starting
     print("Porygon is ready")
 
 
 @client.event
 async def on_member_join(member):
+    # passes name of the member that joined the server
     print(f"{member} has joined the server")
 
 
 @client.event
 async def on_member_remove(member):
+    # passes the name of the member that left the server
     print(f"{member} has left the server")
 
 
 @client.event
 async def on_command_error(ctx, error):
+    # sends the error encountered on the discord
+    # channel called from
     await ctx.send(f"{error}")
 
 
 @client.command()
 async def songsDL(ctx, link):
+    # Downloads the songs from youtube using
+    # youtube-dl and converts them to mp3 and later
+    # uploads them back to the discord channel
+    # upon upload deletes the file from the
+    # pc/server (works with links only)
     await ctx.send("Downloading song(s) (might take sometime to download, be patient.)")
     if audioDL.song_download(link) == 0:
         await ctx.send(f"{ctx.author.mention}Song(s) Downloaded----Uploading now. (just a few more seconds ;) )")
@@ -48,6 +63,8 @@ async def songsDL(ctx, link):
 
 @client.command()
 async def clear(ctx, amount: int = 2):
+    # is used to clear the texts/messages sent
+    # in the discord channel.
     await ctx.channel.purge(limit=amount)
     await ctx.send(f"{amount} message(s) cleared")
     print(f"{amount} message(s) cleared")
@@ -55,6 +72,9 @@ async def clear(ctx, amount: int = 2):
 
 @client.command()
 async def trouble(ctx, member: discord.Member):
+    # Is a functions to trouble discord guild
+    # members only. sends them DMs very frequently
+    # to annoy them.
     if member is not None:
         channel = member.dm_channel
         if channel is None:
@@ -68,6 +88,10 @@ async def trouble(ctx, member: discord.Member):
 
 @client.command()
 async def searchDL(ctx, *, usersearch):
+    # Downloads songs from youtube using
+    # youtube-dl and ytsearch function
+    # and uploads them back to the discord
+    # channel (takes the name/reference of the song)
     await ctx.send("Downloading song")
     if audioDL.search_download(usersearch) == 0:
         await ctx.send(f"{ctx.author.mention} Song Downloaded----Uploading now.")
@@ -78,6 +102,11 @@ async def searchDL(ctx, *, usersearch):
             print(f"{filename} uploaded")
             os.remove(path+filename)
             print(f"{filename} deleted successfully")
+
+
+"""-------------------------------------------------------------------------
+from this point onwards the commands are just for fun in a server.
+They don't have any meaningful purpose."""
 
 
 @client.command()
