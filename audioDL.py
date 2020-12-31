0dcbcc603f89123cfe3d7b50a1129390ec7b1872
Abhijeet3010 <abhijeetsingh2093@gmail.com>
2020-12-31 17:54:55 +0530

    playback via stream or downloaded file

--- audioDL.py
+++ audioDL.py
@@ -2,7 +2,8 @@
 # the discord bot, Porygon.
 
 import os
-
+from youtube_dl import YoutubeDL
+import requests
 
 def song_download(link):
     # Downloads songs via link
@@ -13,5 +14,15 @@ def song_download(link):
 def search_download(usersearch):
     # Downloads songs via searching the most relevant one in the ytsearch.
     command = str(
-        r'youtube-dl -x --audio-format mp3 -o DL_songs/%(title)s.%(ext)s ytsearch:"' + usersearch + r'"')
+        r'youtube-dl -f bestaudio --extract-audio -o DL_songs/%(title)s.%(ext)s ytsearch:"' + usersearch + r'"')
     return os.system(command)
+
+#Get videos from links or from youtube search
+def search_stream(query):
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+        info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+    return (info['title'],info['formats'][0]['url'])
+
+
+if __name__ == "__main__":
+    print(search_stream("castle on the hill"))
--- bot.py
+++ bot.py
@@ -1,36 +1,12 @@
 
 import discord
+from discord import file
 from discord.ext import commands
 import audioDL
 import os
 import key
-import youtube_dl
 import asyncio
 
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    # bind to ipv4 since ipv6 addresses cause issues sometimes
-    'source_address': '0.0.0.0'
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-intents = discord.Intents.default()
-intents.members = True
 
 client = commands.Bot(command_prefix=">")
 
@@ -90,124 +66,52 @@ async def clear(ctx, amount: int = 2):
 
 
 @client.command()
-async def searchDL(ctx, *, usersearch):
+async def playDL(ctx, *, usersearch):
     # Downloads songs from youtube using
     # youtube-dl and ytsearch function
     # and uploads them back to the discord
     # channel (takes the name/reference of the song)
     await ctx.send("Downloading song")
     if audioDL.search_download(usersearch) == 0:
-        await ctx.send(f"{ctx.author.mention} Song Downloaded----Uploading now\N{SLIGHTLY SMILING FACE}.")
+        await ctx.send(f"{ctx.author.mention} Song Downloaded----Playing now\N{SLIGHTLY SMILING FACE}.")
         path = "DL_songs/"
-        filenames = os.listdir(path)
-        channel=ctx.author.voice.channel
-        for filename in filenames:
-            source=discord.FFmpegPCMAudio(path+filename)
-            await channel.connect()
-            ctx.voice_client.play(source)
-            # await ctx.send(file=discord.File(path+filename))
-            # print(f"{filename} uploaded")
-            # os.remove(path+filename)
-            # print(f"{filename} deleted successfully")
+        filename = os.listdir(path)[0]
+        channel = ctx.author.voice.channel
+        source = discord.FFmpegPCMAudio(path+filename)
+        if channel is None:
+            await ctx.send("Please join a voice channel")
+        else:
+            vc = await channel.connect()
+            vc.play(source)
+            while not vc.is_playing():
+                print(f"{filename} deleted")
+                os.remove(filename)
 
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+@client.command()
+async def clearLIB(ctx):
+    path = "DL_songs/"
+    filenames = os.listdir(path)
+    file_index = 0
+    for filename in filenames:
+        os.remove(path+filename)
+        file_index += 1
+    await ctx.send(f"{file_index} file(s) removed")
 
 
-class StreamingCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def join(self, ctx, *, channel: discord.VoiceChannel):
-        """Joins a voice channel"""
-
-        if ctx.voice_client is not None:
-            return await ctx.voice_client.move_to(channel)
-
+@client.command()
+async def play(ctx, *, usersearch):
+    channel = ctx.author.voice.channel
+    if channel is None:
+        await ctx.send("Please join a voice channel")
+    else:
         await channel.connect()
-
-    @commands.command()
-    async def play(self, ctx, *, query):
-        """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(source, after=lambda e: print(
-            'Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(query))
-
-    @commands.command()
-    async def yt(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(player, after=lambda e: print(
-                'Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(player.title))
-
-    @commands.command()
-    async def stream(self, ctx, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            ctx.voice_client.play(player, after=lambda e: print(
-                'Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(player.title))
-
-    @commands.command()
-    async def volume(self, ctx, volume: int):
-        """Changes the player's volume"""
-
-        if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
-
-        ctx.voice_client.source.volume = volume / 100
-        await ctx.send("Changed volume to {}%".format(volume))
-
-    @commands.command()
-    async def stop(self, ctx):
-        """Stops and disconnects the bot from voice"""
-
-        await ctx.voice_client.disconnect()
-
-    @play.before_invoke
-    @yt.before_invoke
-    @stream.before_invoke
-    async def ensure_voice(self, ctx):
-        if ctx.voice_client is None:
-            if ctx.author.voice:
-                await ctx.author.voice.channel.connect()
-            else:
-                await ctx.send("You are not connected to a voice channel.")
-                raise commands.CommandError(
-                    "Author not connected to a voice channel.")
-        elif ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
+    FFMPEG_OPTS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    title, audio_url = audioDL.search_stream(usersearch)
+    source = discord.FFmpegPCMAudio(source=audio_url, **FFMPEG_OPTS)
+    await ctx.send(f'Now playing {title}.')
+    ctx.voice_client.play(source)
 
 
-client.add_cog(StreamingCog(client))
 client.run(key.bot_key)
